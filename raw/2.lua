a74dbd88c6f56888cfe6a033008d52e351b132d1
ForeverOp <gabrielsaulo001@gmail.com>
2026-01-28 03:58:56 -0300

    raw/2.lua

--- raw/2.lua
+++ raw/2.lua
@@ -1,1 +1,6 @@
-
+local StarterGui = game:GetService("StarterGui")
+StarterGui:SetCore("SendNotification", {
+    Title = "Script 2.lua",
+    Text = "Notificação enviada com sucesso!",
+    Duration = 3
+})
