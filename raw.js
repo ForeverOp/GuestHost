57eb24983c1f6984eccd5d745d6f83d881b6f4ec
ForeverOp <gabrielsaulo001@gmail.com>
2026-01-28 03:40:18 -0300

    raw.js

--- raw.js
+++ raw.js
@@ -1,14 +1,13 @@
 import fs from "fs";
 import path from "path";
 
-const dbPath = path.join(process.cwd(), "data/db.json");
-
 export default function handler(req, res) {
   const id = req.query.id;
-  const db = JSON.parse(fs.readFileSync(dbPath));
+  const dbPath = path.join(process.cwd(), "backend/db.json");
+  const db = JSON.parse(fs.readFileSync(dbPath, "utf-8"));
 
   if (!db.scripts[id]) {
-    return res.status(404).send("Not found");
+    return res.status(404).send("Script não encontrado");
   }
 
   res.setHeader("Content-Type", "text/plain");
